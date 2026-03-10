3c7fc30c4f51b7de78e331d68a90320d10ff38ea
Felipe Andrioli <felipeandrioli56@hotmail.com>
2026-03-10 19:28:16 -0300

    Add blink.cmp plugin configuration

--- nvim_with_plugins/lua/config/lazy.lua
+++ nvim_with_plugins/lua/config/lazy.lua
@@ -76,6 +76,120 @@ require("lazy").setup({
 
             end,
         },
+
+        {
+            'saghen/blink.cmp',
+            -- optional: provides snippets for the snippet source
+            dependencies = { 'rafamadriz/friendly-snippets' },
+
+            -- use a release tag to download pre-built binaries
+            version = '1.*',
+            -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+            -- build = 'cargo build --release',
+            -- If you use nix, you can build from source using latest nightly rust with:
+            -- build = 'nix run .#build-plugin',
+
+            ---@module 'blink.cmp'
+            ---@type blink.cmp.Config
+            opts = {
+                -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+                -- 'super-tab' for mappings similar to vscode (tab to accept)
+                -- 'enter' for enter to accept
+                -- 'none' for no mappings
+                --
+                -- All presets have the following mappings:
+                -- C-space: Open menu or open docs if already open
+                -- C-n/C-p or Up/Down: Select next/previous item
+                -- C-e: Hide menu
+                -- C-k: Toggle signature help (if signature.enabled = true)
+                --
+                -- See :h blink-cmp-config-keymap for defining your own keymap
+                keymap = { 
+                    preset = 'default',
+                    ['<C-space>'] = { 
+                        function(cmp) 
+                            cmp.show({ providers = { 'snippets' }})
+                        end, 
+                    },
+                },
+
+                appearance = {
+                    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                    -- Adjusts spacing to ensure icons are aligned
+                    nerd_font_variant = 'mono'
+                },
+
+                cmdline = {
+                    keymap = {
+                        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+                        ['<Tab>'] = { 'show', 'accept' },
+                    },
+                    completion = { menu = { auto_show = true }},
+                },
+
+                -- (Default) Only show the documentation popup when manually triggered
+                completion = { 
+                    accept = { auto_brackets = { enabled = true }},
+
+                    documentation = { 
+                        auto_show = true,
+                        auto_show_delay_ms = 250,
+                        treesitter_highlighting = true,
+                        window = { border = "rounded" },
+                    },
+
+                    list = {
+                        selection = {
+                            preselect = true,
+                            auto_insert = true,
+                        },
+                    },
+
+                    trigger = {
+                        show_on_trigger_character = true,
+                        show_on_blocked_trigger_characters = { ' ', '\n', '\t' },
+                    },
+                },
+
+                -- Default list of enabled providers defined so that you can extend it
+                -- elsewhere in your config, without redefining it, due to `opts_extend`
+
+                sources = {
+                    default = { 'lsp', 'path', 'snippets', 'buffer' },
+                    providers = {
+                        lsp = {
+                            min_keyword_length = 2,
+                            score_offset = 0,
+                        },
+                        path = {
+                            min_keyword_length = 0,
+                        },
+                        snippets = {
+                            min_keyword_length = 2,
+                        },
+                        buffer = {
+                            min_keyword_length = 5,
+                            max_items = 5,
+                        },
+                    },
+                },
+
+                -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+                -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+                -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+                --
+                -- See the fuzzy documentation for more information
+                fuzzy = { 
+                    implementation = "prefer_rust_with_warning",
+                    sorts = {
+                        'score',
+                        'sort_text',
+                        'label',
+                    }
+                }
+            },
+            opts_extend = { "sources.default" }
+        }
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
